fcc24d0b50a33329138a08f6d013b6047e6389d9
John Velis <john@johnvelis.com>
2018-10-12 15:12:03 -0400

    mongoDB add

--- Demo_FileIO_NTier.Solution/DataAccessLayer/JsonDataService.cs
+++ Demo_FileIO_NTier.Solution/DataAccessLayer/JsonDataService.cs
@@ -3,34 +3,38 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using Demo_FileIO_NTier.Models;
+using Newtonsoft;
 using Demo_FileIO_NTier.DataAccessLayer;
+using Newtonsoft.Json;
 
 namespace Demo_FileIO_NTier.DataAccessLayer
 
 {
-    public class XmlDataService : IDataService
+    public class JsonDataService : IDataService
     {
         private string _dataFilePath;
 
         /// <summary>
-        /// read the xml file and load a list of character objects
+        /// read the json file and load a list of character objects
         /// </summary>
         /// <returns>list of characters</returns>
         public IEnumerable<Character> ReadAll()
         {
             List<Character> characters = new List<Character>();
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Character>), new XmlRootAttribute("Characters"));
 
             try
             {
-                StreamReader reader = new StreamReader(_dataFilePath);
-                using (reader)
+                using (StreamReader sr = new StreamReader(_dataFilePath))
                 {
-                    characters = (List<Character>)serializer.Deserialize(reader);
-                }
+                    string jsonString = sr.ReadToEnd();
+
+                    Characters characterList = JsonConvert.DeserializeObject<RootObject>(jsonString).Characters;
+
+                    characters = characterList.Character;
+               }
 
             }
-            catch (Exception)
+            catch (Exception )
             {
                 throw; 
             }
@@ -39,19 +43,23 @@ namespace Demo_FileIO_NTier.DataAccessLayer
         }
 
         /// <summary>
-        /// write the current list of characters to the xml data file
+        /// write the current list of characters to the json data file
         /// </summary>
         /// <param name="characters">list of characters</param>
         public void WriteAll(IEnumerable<Character> characters)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<Character>), new XmlRootAttribute("Characters"));
+            RootObject rootObject = new RootObject();
+            rootObject.Characters = new Characters();
+            rootObject.Characters.Character = characters as List<Character>;
+
+            string jsonString = JsonConvert.SerializeObject(rootObject);
 
             try
             {
                 StreamWriter writer = new StreamWriter(_dataFilePath);
                 using (writer)
                 {
-                    serializer.Serialize(writer, characters);
+                    writer.WriteLine(jsonString);
                 }
             }
             catch (Exception)
@@ -61,7 +69,7 @@ namespace Demo_FileIO_NTier.DataAccessLayer
             }
         }
 
-        public XmlDataService()
+        public JsonDataService()
         {
             _dataFilePath = DataSettings.dataFilePath;
         }
---
+++ Demo_FileIO_NTier.Solution/DataAccessLayer/MongoDBDataService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Demo_FileIO_NTier.Models;
+using Newtonsoft;
+using Demo_FileIO_NTier.DataAccessLayer;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Demo_FileIO_NTier.DataAccessLayer
+
+{
+    public class MongoDBDataService : IDataService
+    {
+
+        /// <summary>
+        /// read the mongoDb collection and load a list of character objects
+        /// </summary>
+        /// <returns>list of characters</returns>
+        public IEnumerable<Character> ReadAll()
+        {
+            List<Character> characters = new List<Character>();
+
+
+
+            try
+            {
+                var client = new MongoClient(
+                    "mongodb://johnvelis:password01@cluster0-shard-00-00-hasci.mongodb.net:27017,cluster0-shard-00-01-hasci.mongodb.net:27017,cluster0-shard-00-02-hasci.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+                    );
+                IMongoDatabase database = client.GetDatabase("cit255");
+                IMongoCollection<Character> characterList = database.GetCollection<Character>("flintstone_characters");
+
+                characters = characterList.Find(Builders<Character>.Filter.Empty).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return characters;
+        }
+
+        /// <summary>
+        /// write the current list of characters to the mongoDb collection
+        /// </summary>
+        /// <param name="characters">list of characters</param>
+        public void WriteAll(IEnumerable<Character> characters)
+        {
+            try
+            {
+                var client = new MongoClient(
+                    "mongodb://johnvelis:password01@cluster0-shard-00-00-hasci.mongodb.net:27017,cluster0-shard-00-01-hasci.mongodb.net:27017,cluster0-shard-00-02-hasci.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+                    );
+                IMongoDatabase database = client.GetDatabase("cit255");
+                IMongoCollection<Character> characterList = database.GetCollection<Character>("flintstone_characters");
+
+                //
+                // delete all documents in the collection
+                //
+                characterList.DeleteMany(Builders<Character>.Filter.Empty);
+
+                characterList.InsertMany(characters);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public MongoDBDataService()
+        {
+            
+        }
+    }
+}
